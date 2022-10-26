5687d46f7873d0066c6db6f9483457b3d792abf4
Labib44 <mdlabib162@gmail.com>
2022-10-26 18:53:51 +0600

    Added package

--- index.js
+++ index.js
@@ -2,21 +2,29 @@ const express = require('express')
 const app = express()
 var cors = require('cors')
 
-const port= process.env.PORT || 5000;
+const port = process.env.PORT || 5000;
 
 app.use(cors())
 
-const categories=require('./Data/categorys.json');
+// const categories = require('./Data/categorys.json');
+const course = require('./Data/course.json');
 
-app.get('/', (req,res)=>{
+
+app.get('/', (req, res) => {
     res.send('Learn will bd API Running');
 });
 
-app.get('/course-category', (req,res)=>{
-    res.send(categories);
+app.get('/course-category', (req, res) => {
+    res.send(course);
 });
 
-app.listen(port, ()=>{
+app.get('/course/:id', (req, res) => {
+    const id = req.params.id;
+    const selectedCourse = course.find(c => c.id == id);
+    res.send(selectedCourse);
+})
+
+app.listen(port, () => {
     console.log('Learn will bd server running on port', port);
 
 });
